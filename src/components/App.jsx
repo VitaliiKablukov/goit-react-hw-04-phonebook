bc9511ffda2ca76b85111ce1a7f5eb75395421c9
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-10-27 23:32:26 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,49 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Section } from './App.styled';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  onSubmitForm = data => {
-    const { name, number } = data;
-    const { contacts } = this.state;
+import { useState, useEffect } from 'react';
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  const onSubmitForm = (name, number) => {
     if (contacts.some(el => el.name === name.toLowerCase())) {
       return alert(`${name} is already contacts`);
     } else {
       const user = { id: nanoid(), name: name, number: number };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, user],
-      }));
+      setContacts(contacts => [...contacts, user]);
     }
   };
-  filterInputText = text => {
+  const filterInputText = text => {
     const filterText = text.toLowerCase();
-    this.setState(() => ({ filter: filterText }));
+    setFilter(filter => filterText);
   };
 
-  onFilter() {
-    const { contacts, filter } = this.state;
+  const onFilter = () => {
     return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
-  }
-  deletecontact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id),
-    }));
   };
-  componentDidMount() {
-    const constacts = localStorage.getItem('contacts');
-    const parseContacts = JSON.parse(constacts);
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
-    }
-  }
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  render() {
-    const visbleContacts = this.onFilter();
-    return (
-      <Section>
-        <h2>Phonebook</h2>
-        <ContactForm onSubmit={this.onSubmitForm} />
-        <h2>Contacts</h2>
-        <Filter filterInputText={this.filterInputText} />
-        <ContactList
-          visbleContacts={visbleContacts}
-          deletecontact={this.deletecontact}
-        />
-      </Section>
-    );
-  }
-}
+  const deletecontact = id => {
+    setContacts(contacts => contacts.filter(el => el.id !== id));
+  };
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  const visbleContacts = onFilter();
+
+  return (
+    <Section>
+      <h2>Phonebook</h2>
+      <ContactForm onSubmit={onSubmitForm} />
+      <h2>Contacts</h2>
+      <Filter filterInputText={filterInputText} />
+      <ContactList
+        visbleContacts={visbleContacts}
+        deletecontact={deletecontact}
+      />
+    </Section>
+  );
+};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,17 @@
-import { Component, Fragment } from 'react';
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
-export class Filter extends Component {
-  filterText = e => {
-    this.props.filterInputText(e.target.value);
+export const Filter = ({ filterInputText }) => {
+  const filterText = e => {
+    filterInputText(e.target.value);
   };
-  render() {
-    return (
-      <Fragment>
-        <label htmlFor="filter">Find contacts by name</label>
-        <input type="text" name="filter" onChange={this.filterText} />
-      </Fragment>
-    );
-  }
-}
+
+  return (
+    <Fragment>
+      <label htmlFor="filter">Find contacts by name</label>
+      <input type="text" name="filter" onChange={filterText} />
+    </Fragment>
+  );
+};
 Filter.propTypes = {
   filterInputText: PropTypes.func.isRequired,
 };
